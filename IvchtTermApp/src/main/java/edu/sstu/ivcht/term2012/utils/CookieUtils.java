9a675cb14d3abacc68231443f7c38abd8b8d5450
romanrw2002 <romanrw2002@gmail.com>
2012-12-03 15:02:32 +0000

    Исправлен NPE

--- IvchtTermApp/src/main/java/edu/sstu/ivcht/term2012/utils/CookieUtils.java
+++ IvchtTermApp/src/main/java/edu/sstu/ivcht/term2012/utils/CookieUtils.java
@@ -25,10 +25,12 @@ public class CookieUtils {
     public static String getCookie(HttpServletRequest request, String cookieName) {
         String searchValue = SITE_PREFIX + cookieName;
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            //если эта искомая cookie
-            if (searchValue.equals(cookie.getName())) {
-                return cookie.getValue();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                //если эта искомая cookie
+                if (searchValue.equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
             }
         }
         return null;
