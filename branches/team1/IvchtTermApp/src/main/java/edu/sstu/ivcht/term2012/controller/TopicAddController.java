073f8c67028b318f72614caaaab09aa5c641f7d5
mga0042@gmail.com <mga0042@gmail.com@b03f4fcb-1a95-ab5b-7935-990cb7d5541a>
2012-12-23 08:53:20 +0000

    Ошибка в web.xml

--- branches/team1/IvchtTermApp/src/main/java/edu/sstu/ivcht/term2012/controller/TopicAddController.java
+++ branches/team1/IvchtTermApp/src/main/java/edu/sstu/ivcht/term2012/controller/TopicAddController.java
@@ -1,7 +1,12 @@
 package edu.sstu.ivcht.term2012.controller;
 
+import javax.servlet.http.HttpServlet;
+
 /**
  * Контроллер-сервлет, вызывающший страницу со списком тем
  */
-public class TopicAddController {
+public class TopicAddController  extends HttpServlet {
+
+    //TODO: сервлет добавления темы
+
 }
