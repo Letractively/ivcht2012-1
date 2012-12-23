a8774016a3ac52d446230ea8b6c7ebee3efa2c73
mga0042@gmail.com <mga0042@gmail.com@b03f4fcb-1a95-ab5b-7935-990cb7d5541a>
2012-12-23 09:20:43 +0000

    Добавление тем пользователем

--- branches/team1/IvchtTermApp/src/main/java/edu/sstu/ivcht/term2012/controller/TopicAddController.java
+++ branches/team1/IvchtTermApp/src/main/java/edu/sstu/ivcht/term2012/controller/TopicAddController.java
@@ -1,12 +1,70 @@
 package edu.sstu.ivcht.term2012.controller;
 
+import edu.sstu.ivcht.term2012.model.Topic;
+import edu.sstu.ivcht.term2012.service.ITopicsService;
+import edu.sstu.ivcht.term2012.utils.ServiceInstancer;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * Контроллер-сервлет, вызывающший страницу со списком тем
  */
 public class TopicAddController  extends HttpServlet {
 
-    //TODO: сервлет добавления темы
+    //Получение службы работы с темами
+    ITopicsService _topicService = ServiceInstancer.getTopicService();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    /**
+     * Обработка запроса со страницы topicAdd.jsp
+     * @param req
+     * @param resp
+     * @throws IOException
+     * @throws ServletException
+     */
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        req.setCharacterEncoding("UTF-8");
+
+        //Извлечение переданных сервлетом переменных
+        String subject = req.getParameter("subject");
+        if (subject=="")
+             subject="<empty>";
+        String description = req.getParameter("description");
+
+        //Попытка добавления темы в БД
+        try {
+            //Новый экземпляр темы
+            Topic topic = new Topic(
+                    subject,
+                    description,
+                    new Date()
+            );
+
+            _topicService.addTopic(topic);
+
+            req.setAttribute("result", "Тема успешно добавлена");
+
+        } catch (Exception e) {
+            req.setAttribute("error", "Произошла ошибка при создании темы");
+            e.printStackTrace();
+        }
+
+        //Передает результаты обработки запроса на страницу
+        getServletContext().getRequestDispatcher("/topicAdd.jsp").forward(req, resp);
+    }
 
 }
